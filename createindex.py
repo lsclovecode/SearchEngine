9e92df21c31bb01150199fd69fc186255644d9f3
HaoNi <haoni@dhcp-v051-218.mobile.uci.edu>
2017-03-07 21:31:40 -0800

    doc length calculation and mongodb storage

--- createindex.py
+++ createindex.py
@@ -8,14 +8,19 @@ import re
 import sys
 import string
 import json
+import math
+from pymongo import MongoClient
 reload(sys)
 #####
 sys.setdefaultencoding('utf-8')
 
 index = defaultdict(list)
 numberOfDocuments = 0
-finalindex = {}
 
+
+client = MongoClient('localhost', 27017)
+db = client.test_database
+collection = db.tdIndex
 
 
 def tokenize(text):
@@ -42,7 +47,7 @@ def parseWordList(tokens):
 
 
 for i in range(1):
-    for j in range(20):
+    for j in range(3):
         docid = 500 * i + j
         try:
             soup = BeautifulSoup(open(str(i) + '/' + str(j)), 'lxml')
@@ -84,30 +89,52 @@ for i in range(1):
 
 
 '''{'_word_':{'df':3, 'docs':['_docid_':{'tf':5, 't': 0/1, 'pos':[5,7]}} ]     }'''
-
+docIndex = {} #doc normalize length
 
 for term, doc in index.iteritems():
+    finalindex = {}
     df = len(doc)
-    finalindex[term] = {}
-    finalindex[term]['df'] = df
-    finalindex[term]['docs'] = []
+    finalindex["_id"] = term
+    finalindex['df'] = df
+    finalindex['docs'] = []
     for eachdoc in doc:
         eachdocsummary = {}
         docid = eachdoc[0]
-        eachdocsummary[docid] = {}
+        eachdocsummary["docid"] = docid
         tf = 0
         if eachdoc[1][0][0] == 1:
             tf = 20 + len(eachdoc[1][1])
-            eachdocsummary[docid]['t'] = 1
+            eachdocsummary['t'] = 1
         elif eachdoc[1][0][0] == 0:
             tf = len(eachdoc[1][1])
-            eachdocsummary[docid]['t'] = 0
-        eachdocsummary[docid]['tf'] = tf
-        eachdocsummary[docid]['p'] = eachdoc[1][1]
-        finalindex[term]['docs'].append(eachdocsummary)
+            eachdocsummary['t'] = 0
+        eachdocsummary['tf'] = tf
+        eachdocsummary['p'] = eachdoc[1][1]
+        tfidf = (1 + math.log(tf)) * (math.log(500.0 / df))
+        eachdocsummary['tfidf'] = tfidf
+        if docid in docIndex:
+            docIndex[docid] += tfidf * tfidf
+        else:
+            docIndex[docid] = tfidf * tfidf
+        finalindex['docs'].append(eachdocsummary)
+    collection.insert_one(finalindex)
+
+doccollection = db.docInfo
+for docid in docIndex:
+    record = {}
+    record['_id'] = docid
+    record['length'] = math.sqrt(docIndex[docid])
+    doccollection.insert_one(record)
+
+'''
+print 'docindex /n', docIndex
+
 
 print 'total valid documents', numberOfDocuments
-
-
+ 
+ 
 with open('test.json', 'w') as ff:
-    json.dump(finalindex, ff, indent=2)
+   json.dump(finalindex, ff, indent=2)
+
+
+'''
--- notice.md
+++ notice.md
@@ -1,1 +1,13 @@
 * write what you have done here
+
+# to search word laptop and return doc list
+client = MongoClient('localhost', 27017)
+db = client.test_database
+collection = db.tdIndex
+collection.find_one({"_id": "laptop"},{"docs":1})
+
+#to search docid=1's length;
+client = MongoClient('localhost', 27017)
+db = client.test_database
+collection = db.docInfo
+collection.find_one({"_id": 1},{"length":1})
