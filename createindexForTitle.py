40f6d23a2edb3312e198fb75c2fbaa591a85adff
lsclovecode <liusicong999@gmail.com>
2017-03-11 23:13:49 -0800

    search engine

--- createindexForTitle.py
+++ createindexForTitle.py
@@ -8,7 +8,6 @@ stemmer = SnowballStemmer("english")
 import re
 import sys
 import string
-import json
 import math
 from pymongo import MongoClient
 
@@ -20,7 +19,7 @@ numberOfDocuments = 0
 
 client = MongoClient('localhost', 27017)
 db = client.test_database
-collection = db.tdIndex
+collection = db.titleIndex
 
 
 def tokenize(text):
@@ -57,13 +56,6 @@ for i in range(75):
             except:
                 print 'folder', i, 'doc', j, "is not a valid html"
                 continue
-            for script in soup(["script", "style"]):
-                script.decompose()
-            fulltext = soup.get_text().encode('utf-8')
-            fulltext_tokenized = tokenize(fulltext)
-            fulltext_list = parseWordList(fulltext_tokenized)
-            numberOfDocuments += 1
-
             try:
                 title = soup.title.text
                 titlelist = parseWordList(tokenize(title))
@@ -73,68 +65,27 @@ for i in range(75):
                 titlelist = []
             termdict = {}
 
-            for position, term in enumerate(fulltext_list):
+            for position, term in enumerate(titlelist):
                 if not term in stopwords.words('english'):
                     try:
                         termdict[term][1][1].append(position)
                     except:
-                        if term in titlelist:
-                            termdict[term] = [docid, [[1], [position]]]
-                        else:
-                            termdict[term] = [docid, [[0], [position]]]
+                        termdict[term] = [docid, [[1], [position]]]
             for term, postingpage in termdict.iteritems():
                 index[term].append(postingpage)
         except:
             print 'folder', i, 'doc', j, "It's a not valid html"
             continue
 
-'''{'_word_':{'df':3, '_docid_':{'tf':5, 't': 0/1, 'pos':[5,7]}     }     }'''
-docIndex = {}  # doc normalize length
-
 for term, doc in index.iteritems():
     finalindex = {}
     df = len(doc)
     finalindex["_id"] = term
-    finalindex['df'] = df
     finalindex["docid"] = {}
     for eachdoc in doc:
         docid = eachdoc[0]
-        finalindex["docid"][str(docid)] = {}
-        tf = 0
-        if eachdoc[1][0][0] == 1:
-            tf = 10000 + len(eachdoc[1][1])
-            finalindex["docid"][str(docid)]['t'] = 1
-        elif eachdoc[1][0][0] == 0:
-            tf = len(eachdoc[1][1])
-            finalindex["docid"][str(docid)]['t'] = 0
-        finalindex["docid"][str(docid)]['tf'] = tf
-        finalindex["docid"][str(docid)]['p'] = eachdoc[1][1]
-        tfidf = (1 + math.log(tf)) * (math.log(37438.0 / df))
-        finalindex["docid"][str(docid)]['tfidf'] = tfidf
-        if docid in docIndex:
-            docIndex[docid] += tfidf * tfidf
-        else:
-            docIndex[docid] = tfidf * tfidf
+        finalindex["docid"][str(docid)] = eachdoc[1][1]
     collection.insert_one(finalindex)
-print "finished part1"
-doccollection = db.docInfo
-
-for docid in docIndex:
-    record = {}
-    record['_id'] = docid
-    record['length'] = math.sqrt(docIndex[docid])
-    doccollection.insert_one(record)
-
-print "finished part2"
-'''
-print 'docindex /n', docIndex
 
 
-print 'total valid documents', numberOfDocuments
-
-
-with open('test.json', 'w') as ff:
-   json.dump(finalindex, ff, indent=2)
-
-
-'''
+print "finished"
--- queryindex.py
+++ queryindex.py
@@ -1,14 +1,54 @@
+from bs4 import BeautifulSoup
+from nltk.tokenize import word_tokenize
+import re
 import math
+import numpy as np
+import time
+from nltk.stem import SnowballStemmer
+stemmer = SnowballStemmer("english")
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.test_database
+index = db.tdIndex
+length = db.docInfo
+idurl = db.urlIndex
+titleindex = db.titleIndex
 
 
-# d is a dictionary
+
+def tokenize(text):
+    try:
+        text_tokenized = word_tokenize(text)
+    except:
+        text = unicode(text, errors='ignore')
+        text_tokenized = word_tokenize(text)
+    return text_tokenized
+
+
+def parseWordList(tokens):
+    tokenlist = []
+    for token in tokens:
+        try:
+            token = token.translate(string.punctuation)
+        except:
+            token = token
+        m = re.match(r'^[a-zA-Z]+$', token)
+        if m:
+            #if not token in stopwords.words('english'):
+            tokenlist.append(token.encode('utf-8'))
+    return tokenlist
+
+
 def parseQuery(q):
     q = q.strip()
-    return q.spilt(" ")
-
+    termlist = q.split(" ")
+    termlist = [stemmer.stem(term).encode('utf-8') for term in termlist]
+    return termlist
 
 def getDocsHaveThisWord(term):  # given a single term, return all docs that contains this term
-    return [int(x.keys()[0]) for x in d[term]['docs']]  # d[term]['doc'] returns a list of dictionaries
+
+    d = index.find_one({"_id": term})
+    return [int(x) for x in d['docid'].keys()]  # d[term]['doc'] returns a list of dictionaries
 
 
 def intersectLists(lists):  # given a list of each term's list, find the list which contains all the terms
@@ -16,15 +56,13 @@ def intersectLists(lists):  # given a list of each term's list, find the list wh
         return []
     # start intersecting from the smaller list
     lists.sort(key=len)
-    return list(reduce(lambda x, y: set(x) & set(y), lists))
+    result = list(reduce(lambda x, y: set(x) & set(y), lists))
+    return sorted(result)
 
 
 def getPosition(term, docid):
-    posLists = []
-    for eachdoc in d[term]['docs']:
-        if eachdoc.has_key(docid):
-            posLists.append(eachdoc[docid]['p'])
-    return posLists
+    d = index.find_one({"_id": term})
+    return d['docid'][str(docid)]['p']
 
 
 # give a query, first parse it into term list, for each term find the docs that contains the term,
@@ -38,14 +76,15 @@ def checkNGramPosition(posLists):  # given a sequence of each term's position li
 
 
 def textQueryDocs(termList):
+    lists = []
     for term in termList:
         lists.append(getDocsHaveThisWord(term))
     docList = intersectLists(lists)
     return docList
 
 
-def phaseQueryDocs(termList):  # given a list of terms
-    # lists = []
+def phaseQueryDocs1Param(termList):  # given a list of terms
+    lists = []
     for term in termList:
         lists.append(getDocsHaveThisWord(term))
     docList = intersectLists(lists)  # returns a list that contains all the terms
@@ -58,38 +97,106 @@ def phaseQueryDocs(termList):  # given a list of terms
         if len(result) != 0:
             finalList.append(docid)
     return finalList
+
+def phaseQueryDocs2Param(termList, docList):  # given a list of terms
+    finalList = []
+    for docid in docList:
+        temp = []
+        for term in termList:
+            temp.append(getPosition(term, docid))
+        result = checkNGramPosition(temp)
+        if len(result) != 0:
+            finalList.append(docid)
+    return finalList
+
 # vector1, vector2 are two lists with same length
 # length is the length of a doc vector
 
 # dic1 of query tf-idf
 # dic2 of document tf-idf
+'''The query may contain repetitive words'''
 def getQueryVector(termList):
     queryvector = []
     querylength = 0
     for term in termList:
-        queryvector.append(dic1[term])
-        querylength = querylength + dic1[term] * dic1[term]
+        df = index.find_one({"_id": term})['df']
+        df = math.log(37438.0 / df)
+        queryvector.append(df)
+        querylength += df * df
     querylength = math.sqrt(querylength)
     for i in range(len(queryvector)):
         queryvector[i] = queryvector[i] / querylength
+    return queryvector
 
-def computeCosineSimilarity(queryvector, docvector, doclength):
-    vectorsum = 0
-    for i in range(len(queryvector)):
-        vectorsum = queryvector[i] * docvector[i] + vectorsum
-    return vectorsum / math.sqrt(doclength)
+def getDocVector(termList, docid):
+    docVector = []
+    try:
+        docLength = length.find_one({"_id": docid})['length']
+    except:
+        docLength = 5000
+    for term in termList:
+        d = index.find_one({"_id": term})
+        tfidf = d['docid'][str(docid)]['tfidf'] / docLength
+        docVector.append(tfidf)
+    return docVector
 
+
+def computeCosineSimilarity(queryvector, docvector):
+    q = np.array(queryvector)
+    d = np.array(docvector)
+    resultlist = (q * d).tolist()
+    return sum(resultlist)
+
+def checkATermInTitle(term, docid):
+    d = index.find_one({"_id": term})
+    return d['docid'][str(docid)]['t'] == 1
+
+def checkTermListInTitle(termList, docid):
+    for term in termList:
+        if not checkATermInTitle(term, docid):
+            return False
+    return True
+
+def findDocsInTitle(termList, docList):
+    result = []
+    for doc in docList:
+        if checkTermListInTitle(termList, doc):
+            result.append(doc)
+    return result
+
+def findDocsInTitleByTitleIndex(termList):
+    lists = []
+    for term in termList:
+        d = titleindex.find_one({"_id": term})
+        lists.append([int(x) for x in d['docid'].keys()])
+    docList = intersectLists(lists)
+
+    finalList = []
+    for docid in docList:
+        temp = []
+        for term in termList:
+            d = titleindex.find_one({"_id": term})
+            temp.append(d['docid'][str(docid)])
+        result = checkNGramPosition(temp)
+        if len(result) != 0:
+            finalList.append(docid)
+    return finalList
 
 # docList is a list of docids, you should calculate the similarity of the qvecor of each docid's vector
 # then rank, return a ranked list
-def rankDocuments(docList, termList):
+def rankDocuments(docList, termList, queryvector):
     scoredic = {}
-    queryvector = getQueryVector(termList)
+    #print queryvector
+
     for docid in docList:
-        doclength, docvector = dic2[docid][termList]
-        score = computeCosineSimilarity(queryvector, docvector, doclength)
+        docvector = getDocVector(termList, docid)
+        #print docvector
+        score = computeCosineSimilarity(queryvector, docvector)
+        #print "score", score
         scoredic[score] = docid
-    sortedscore = sorted(scoredic.keys(), cmp=None, key=None, reverse=False)
+
+    sortedscore = sorted(scoredic.keys(), cmp=None, key=None, reverse=True)
+    #print sortedscore
     rank = []
     for i in sortedscore:
         rank.append(scoredic[i])
@@ -98,4 +205,69 @@ def rankDocuments(docList, termList):
 
 
 
+def search(input):
+    termList = parseQuery(input)
+    #print termList
+    queryvector = getQueryVector(termList)
+    #print queryvector
+    docList = textQueryDocs(termList)
+    #print docList
+    #docsInTitle = findDocsInTitle(termList, docList)
+    docsInTitle = findDocsInTitleByTitleIndex(termList)
+    #print len(docsInTitle)
+    #print docsInTitle
+    result = []
+    if len(docsInTitle) >= 5:
+        result = rankDocuments(docsInTitle, termList, queryvector)
+        return result[:5]
+    else:
+        remaining = list(set(docList) - set(docsInTitle))
+        remainingWithPositionChecked = phaseQueryDocs2Param(termList, remaining)
+        result1 = rankDocuments(docsInTitle, termList, queryvector)
+        if len(remainingWithPositionChecked) != 0:
+            result2 = rankDocuments(remainingWithPositionChecked, termList, queryvector)
+        else:
+            result2 = rankDocuments(remaining, termList, queryvector)
+        result = result1 + result2
+        if len(result) >= 5:
+            return result[:5]
+    return result
+#words = ["mondego", "software engineering", "security", "student affairs", "graduate courses", "crista lopes", "REST", "computer games", "information retrieval", "machine learning"]
+
+
+word = raw_input()
+t1 = time.clock()
+result = search(word)
+t2 = time.clock()
+print t2 - t1, "seconds"
+print result
+
+term = parseQuery(word)[0]
+
+for id in result:
+    i = id / 500
+    j = id % 500
+    pos = getPosition(term, id)[0]
+    with open(str(i) + '/' + str(j)) as f:
+        soup = BeautifulSoup(f, 'lxml')
+    try:
+        body = soup.body.text
+    except:
+        print ""
+    for script in soup(["script", "style"]):
+        script.decompose()
+    fulltext = soup.get_text().encode('utf-8')
+    fulltext_tokenized = tokenize(fulltext)
+    fulltext_list = parseWordList(fulltext_tokenized)
+    if pos < 3:
+        demo = fulltext_list[pos: pos + 5]
+    else:
+        demo = fulltext_list[pos - 2: pos + 5]
+    s = ''
+    for word in demo:
+        s += ' ' + word
+    urlname = idurl.find_one({"id": id})['url']
+    if "ics" in urlname:
+        print "http://" + urlname
+        print s
 
